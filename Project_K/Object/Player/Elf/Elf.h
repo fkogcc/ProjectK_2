a325dc46b325f7a625165e0b34d193cf59761137
sayuGitHub <114457066+sayudesu@users.noreply.github.com>
2023-04-27 18:59:29 +0900

    Elf修正

--- Project_K/Object/Player/Elf/Elf.h
+++ Project_K/Object/Player/Elf/Elf.h
@@ -2,6 +2,7 @@
 
 #include "../Base/PlayerBase.h"
 
+class ElfIdol;
 class Elf : public PlayerBase
 {
 public:
@@ -12,7 +13,12 @@ public:
 	void End   ();// I—¹ˆ—
 	void Update();// XV
 	void Draw  ();// •`‰æ
-private:
 
+private:
+	int m_handle;
+
+	int m_imageX, m_imageY;
+
+	ElfIdol* m_pIdle;
 };
 
