c0ce701b0b122a8680ab0975bedf7fdb86f61f81
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2023-04-27 17:55:12 +0900

    攻撃２状態の更新処理追加

--- Project_K/Object/Player/Kinnikurou/KinnikuMuscle.h
+++ Project_K/Object/Player/Kinnikurou/KinnikuMuscle.h
@@ -3,5 +3,12 @@
 
 class KinnikuMuscle : public MoveTypeBase
 {
+public:
+	KinnikuMuscle();
+	virtual ~KinnikuMuscle();
+
+	void Init();
+	void End();
+	void Update(int imagePosX, int imagePosY);
 };
 
