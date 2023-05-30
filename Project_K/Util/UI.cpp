cffceb7d1ade1f9d88b78df1919c8560f9df70ab
S0624 <mmasyumaro.012345@gmail.com>
2023-05-30 16:52:18 +0900

    UIの修正
    修正しました遅れました

--- Project_K/Util/UI.cpp
+++ Project_K/Util/UI.cpp
@@ -8,60 +8,83 @@ namespace
 	const int kHightGauge = 40;
 	const int kTimeBoxSize = 50;// 時間を表示する場所のボックスのサイズ
 	const int kCenter = Game::kScreenWidth / 2;// 真ん中の位置
+	const char* kFont = "HGP行書体";
+	int a = 0;
 }
 
-UI::UI()
+UI::UI(int Hp1, int Hp2) :
+	m_boxPos(kCenter, 90),	//時間を表示するボックスの初期化
+	m_countFrame(50),		// タイマーの初期化
+	m_attackFlag(false)		//攻撃中かしるためのフラグ
 {
-	m_boxPos = { kCenter,90 };//時間を表示するボックスの初期化
+	a = CreateFontToHandle(kFont, 70, -1, -1);
 	// 1Pの初期化
 	UI1.m_pos = { m_boxPos.x - kTimeBoxSize,80 };
-	UI1.m_life = 100;
-	UI1.m_temp = 0;
+	UI1.m_temp = Hp1;
+	UI1.m_life = UI1.m_temp;
+	UI1.m_lower = UI1.m_life;
 	// 2Pの初期化
 	UI2.m_pos = { m_boxPos.x + kTimeBoxSize,80 };
-	UI2.m_life = 100;
-	UI2.m_temp = 0;
-	m_countFrame = 50;// タイマーの初期化
+	UI2.m_temp = Hp2;
+	UI2.m_life = UI2.m_temp;
+	UI2.m_lower = UI2.m_life;
 }
 
 void UI::Update()
 {
-	m_countFrame++;//タイマーのカウント
-	if (m_countFrame >= 40)
-	{
-		UI1.m_temp = UI1.m_life;// 仮のHPを今のHP似合わせる
-		UI2.m_temp = UI2.m_life;// 仮のHPを今のHP似合わせる
-		m_countFrame = 0;
-	}
-	if (UI1.m_temp == UI1.m_life && UI2.m_temp == UI2.m_life)
-	{
-		m_countFrame = 0;
-	}
-
-	if (UI1.m_life <= 0)
-	{
-		UI1.m_temp = 0;
-		UI1.m_life = 0;
-	}
-	if (UI2.m_life <= 0)
-	{
-		UI2.m_temp = 0;
-		UI2.m_life = 0;
-	}
-	printfDx("%d\n", UI1.m_life);
+	HpUpdate();
 }
 
 void UI::Draw()
 {
+	HpDraw();
+	FontDraw();
+}
+
+void UI::HpUpdate()
+{
+	m_countFrame++;//タイマーのカウント
+	if (UI1.m_temp >= 0)
+	{
+		UI1.m_life = UI1.m_temp;
+	}
+	if (UI2.m_temp >= 0)
+	{
+		UI2.m_life = UI2.m_temp;
+	}
+	if (m_countFrame >= 40 && !m_attackFlag)
+	{
+		UI1.m_lower = UI1.m_life;// 仮のHPを今のHP似合わせる
+		UI2.m_lower = UI2.m_life;// 仮のHPを今のHP似合わせる
+		m_countFrame = 0;
+	}
+	if (UI1.m_lower != UI1.m_life && UI2.m_lower != UI2.m_life)
+	{
+		m_countFrame = 0;
+	}
+	if (UI1.m_life <= 0)
+	{
+		UI1.m_lower = 0;
+		UI1.m_life = 0;
+	}
+	if (UI2.m_life <= 0)
+	{
+		UI2.m_lower = 0;
+		UI2.m_life = 0;
+	}
+}
+
+void UI::HpDraw()
+{
 	//DrawLine(Game::kScreenWidth / 2, 0, Game::kScreenWidth / 2, 600, 0xffffff);
 	//仮のHPの表示
-	DrawBox(static_cast<int>(UI1.m_pos.x), static_cast<int>(UI1.m_pos.y), 
-		static_cast<int>(UI1.m_pos.x) - (5 * UI1.m_temp), static_cast<int>(UI1.m_pos.y) + kHightGauge, 0xff0000, true);
+	DrawBox(static_cast<int>(UI1.m_pos.x), static_cast<int>(UI1.m_pos.y),
+		static_cast<int>(UI1.m_pos.x) - (5 * UI1.m_lower), static_cast<int>(UI1.m_pos.y) + kHightGauge, 0xff0000, true);
 	DrawBox(static_cast<int>(UI1.m_pos.x), static_cast<int>(UI1.m_pos.y),
 		static_cast<int>(UI1.m_pos.x) - (5 * UI1.m_life), static_cast<int>(UI1.m_pos.y) + kHightGauge, 0x00ff00, true);
 	//現在HPの表示
 	DrawBox(static_cast<int>(UI2.m_pos.x), static_cast<int>(UI2.m_pos.y),
-		static_cast<int>(UI2.m_pos.x) + (5 * UI2.m_temp), static_cast<int>(UI2.m_pos.y)+ kHightGauge, 0xff0000, true);
+		static_cast<int>(UI2.m_pos.x) + (5 * UI2.m_lower), static_cast<int>(UI2.m_pos.y) + kHightGauge, 0xff0000, true);
 	DrawBox(static_cast<int>(UI2.m_pos.x), static_cast<int>(UI2.m_pos.y),
 		static_cast<int>(UI2.m_pos.x) + (5 * UI2.m_life), static_cast<int>(UI2.m_pos.y) + kHightGauge, 0x00ff00, true);
 
@@ -71,4 +94,15 @@ void UI::Draw()
 	DrawBox(static_cast<int>(m_boxPos.x) - kTimeBoxSize, static_cast<int>(m_boxPos.y) - kTimeBoxSize,
 		static_cast<int>(m_boxPos.x) + kTimeBoxSize, static_cast<int>(m_boxPos.y) + kTimeBoxSize, 0xff00f0, false);
 
+}
+
+void UI::FontUpdate()
+{
+}
+
+void UI::FontDraw()
+{
+	DrawFormatStringToHandle((Game::kScreenWidth -
+		GetDrawStringWidthToHandle("%d", 11, a)) / 2,
+		100 - 40, 0x7fffff, a, "%d", 99);
 }
--- Project_K/Util/UI.h
+++ Project_K/Util/UI.h
@@ -4,15 +4,23 @@
 class UI
 {
 public:
-	UI();
+	UI(int Hp1, int Hp2);
 	~UI() {};
 
 	void Init() {}// 初期化
 	void Update();// 更新処理
 	void Draw();// 描画処理
 
-	void GetHp1(int hp) { UI1.m_life = hp; }// 1PのHP
-	void GetHp2(int hp) { UI2.m_life = hp; }// 2PのHP
+	void HpUpdate();
+	void HpDraw();
+
+	void FontUpdate();
+	void FontDraw();
+
+
+	void GetHp1(int hp) { UI1.m_temp = hp; }// 1PのHP
+	void GetHp2(int hp) { UI2.m_temp = hp; }// 2PのHP
+	void AttackFlag(bool flag) { m_attackFlag = flag; }
 private:
 
 	Vec2 m_boxPos;
@@ -20,13 +28,14 @@ private:
 	struct  UIData
 	{
 		Vec2 m_pos = { 0,0 };// HPバーの位置
+		int m_temp = 0;// HPをうけとる用の変数
 		int m_life = 0;// 各プレイヤーのHP
-		int m_temp = 0;// 仮で保存するHP用の変数
+		int m_lower = 0;// 仮で保存するHP用の変数
 	};
 
 	int m_countFrame;// 仮のHPを減らすタイマー
 
-
+	bool m_attackFlag;
 	UIData UI1;
 	UIData UI2;
 };
