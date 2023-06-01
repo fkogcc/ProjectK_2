e73e21ce61c32d88737ee85e890bbce7e53e995e
niyugitHub <ryutansmr@icloud.com>
2023-06-01 14:00:06 +0900

    CharaChoiceにコメント追加

--- Project_K/System/CharaChoice.h
+++ Project_K/System/CharaChoice.h
@@ -16,22 +16,24 @@ public:
 	void Update();
 	void Draw();
 
+	//キャラナンバーを返す
 	int GetChackChara1() { return m_charaNumber1; }
 	int GetChackChara2() { return m_charaNumber2; }
 
+	//キャラクター決定情報を返す.
 	bool GetDecision1() { return m_Decision1; }
 	bool GetDecision2() { return m_Decision2; }
 
 private:
+	//キャラナンバーのアップデート
 	void updateChara1();
 	void updateChara2();
 
-	int m_choiceNum1;
-	int m_choiceNum2;
-
+	// キャラナンバー(0:恐竜 1:きんに君2:エルフ3:魔女)
 	int m_charaNumber1;
 	int m_charaNumber2;
 
-	bool m_Decision1;
-	bool m_Decision2;
+	
+	bool m_Decision1;// キャラクター1が決定されたか
+	bool m_Decision2;// キャラクター2が決定されたか
 };
