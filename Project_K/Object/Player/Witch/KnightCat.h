7af34ce2fe2b699af19ea1d099698676934226be
S0624 <mmasyumaro.012345@gmail.com>
2023-06-08 15:56:51 +0900

    当たり判定もろもろの修正
    コメントの追加
    当たり判定の修正
    なんかいろいろしました

--- Project_K/Object/Player/Witch/KnightCat.h
+++ Project_K/Object/Player/Witch/KnightCat.h
@@ -4,26 +4,28 @@ class KnightCat :
 	public MoveTypeBase
 {
 public:
-	KnightCat();
-	virtual ~KnightCat();
-	void Init();
-	void Update();
-	void Draw();
+	KnightCat();// コンストラクタ
+	virtual ~KnightCat();// デストラクタ
+	void Init();// 初期化
+	void Update();// 更新処理
+	void Draw();// 描画処理
 
-	void SetReversal(bool reversal) { m_reversal = reversal; }
-	void SetPos(Vec2 pos) { m_pos = pos; }
-	void SetFlag(bool flag) { m_exist = flag; }
-	bool IsExist() { return m_exist; }
-	Vec2 ReturnPos() { return m_pos; }
+	void SetReversal(bool reversal) { m_reversal = reversal; }// 反転しているか
+	void SetPos(Vec2 pos) { m_pos = pos; }// プレイヤーの位置
+	void SetFlag(bool flag) { m_exist = flag; }// 存在しているかのフラグをセット
+	bool IsExist() { return m_exist; }// 存在しているかのフラグ
+	float GetMovePos() { return m_movePos; }// 猫ちゃんの位置
+	Vec2 GetPos() { return m_pos; }// プレイヤーの位置を記憶する
 private:
-	int m_handle;
-	bool m_reversal;
-	Vec2 m_pos;
-	int m_timeCount;
-	int m_animeFrame;
-	int m_animeMax;
-	int m_animeWidth;
-	int m_animeHight;
-	bool m_exist;
+	int m_handle;// 画像
+	bool m_reversal;// 反転
+	Vec2 m_pos;// 位置
+	float m_movePos;// 動いた位置
+	int m_timeCount;// 時価運のカウントをする変数
+	int m_animeFrame;// フレーム
+	int m_animeMax;// アニメーションの末尾番号
+	int m_animeWidth;// アニメーションのいち
+	int m_animeHight;// アニメーションの地位
+	bool m_exist;// 存在
 };
 
