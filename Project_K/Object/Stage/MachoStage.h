ba3331e826c93c0ffa8ca27e4890212ad258d5ce
Gaxx <lwt5gy707g@gmail.com>
2023-06-19 11:37:05 +0900

    マッチョのステージグラフィック変更

--- Project_K/Object/Stage/MachoStage.h
+++ Project_K/Object/Stage/MachoStage.h
@@ -1,16 +1,16 @@
 #pragma once
-#include "../Stage/StageBase.h"
-
-class MachoStage : public StageBase
-{
-public:
-	MachoStage(int stageNo);
-
-	void Init();
-	void Update();
-	void Draw();
-
-private:
-	int m_handle;	// グラフィックハンドル
-};
+//#include "../Stage/StageBase.h"
+//
+//class MachoStage : public StageBase
+//{
+//public:
+//	MachoStage(int stageNo);
+//
+//	void Init();
+//	void Update();
+//	void Draw();
+//
+//private:
+//	int m_handle;	// グラフィックハンドル
+//};
 
